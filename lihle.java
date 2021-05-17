827db90ff055753589e930113e55f53850c671dd
LihleMbilini <84271201+LihleMbilini@users.noreply.github.com>
2021-05-17 02:38:17 +0200

    Add files via upload

---
+++ lihle.java
@@ -0,0 +1,15 @@
+/* 
+  Full Names  : Lihle Mbili 
+
+  Student number : 219383685
+
+  Topic : Assigmnent 2 java Github project
+
+*/
+import javax.swing.*;
+public class lihle {
+    public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Programming is fun",null, JOptionPane.INFORMATION_MESSAGE);
+     System.exit(0);
+    } 
+}
